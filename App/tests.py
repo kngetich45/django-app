98e6540f3b0a47ca9d75bf23661388435b4dd735
Ngetich Benard <kngetich45@gmail.com>
2022-07-13 11:02:43 +0300

    ns

--- App/tests.py
+++ App/tests.py
@@ -1,3 +1,14 @@
+from unicodedata import name
 from django.test import TestCase
+from . models import Member
 
 # Create your tests here.
+class ModelTesting(TestCase):
+    
+    def setUp(self):
+        self.app = Member.objects.create(name='django',username='django',email='django',phone='django',age='django',location='django',gender='django',bio='django')
+
+
+    def test_post_model(self):
+        d = self.app
+        self.assertTrue(isinstance(d, Member))
